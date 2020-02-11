8be62421eb527817a2e3b946f1b7db1c2a9348f7
Lester Young <lyoung070789@gmail.com>
2020-02-11 00:01:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,8 +12,10 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma(phrase)
+def speak_to_grandma (phrase)
   if phrase != phrase.upcase
-    No, NOT SINCE 1938!
-    
+    "HUH?! SPEAK UP, SONNY!"
+  else
+    "NO, NOT SINCE 1938!"
+  end
 end
